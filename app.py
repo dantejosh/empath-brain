e7cba47a84419d1249300dfc335777f8d1dd7d75
dantejosh <56048073+dantejosh@users.noreply.github.com>
2026-02-18 01:55:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+try:
+    from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+    VADER_OK = True
+except Exception as e:
+    VADER_OK = str(e)
+
+
 @app.route("/")
 def root():
     return "boot ok"
 
+
 @app.route("/emotion")
 def emotion():
-    return jsonify({"index": 50, "summary": "boot test"})
+    return jsonify({
+        "vader_status": VADER_OK
+    })
